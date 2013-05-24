4171ff3a8732976c621df38a16747ae07649b80b
Oskar Kapala <oskar.kapala@gmail.com>
2013-05-24 22:40:00 +0200

    Krok drugi tzn pierwszy: Suite

--- src/test/scala/org/kata/test/CarSuite.scala
+++ src/test/scala/org/kata/test/CarSuite.scala
@@ -9,27 +9,5 @@ import Car._
  *         A suite of tests. A Suite instance encapsulates a conceptual suite (i.e., a collection) of tests.
  *
  */
-// Scenariusz
-// step-1-1: szkielet
-//class CarSuite extends Suite
-
-// step-1-2: gotowe
-class CarSuite extends Suite {
-
-  def testStartSpeed() {
-
-    val car = basicCar
-
-    val state: Car.CarState = car.startEngine
-    assert(state._2 == 0)
-
-    val fakeState = (0, 0)
-    //    val fakeState = (0,43)
-    assert(fakeState._2 == 0, "Speed must be zero but is " + fakeState._2)
-
-    assert(fakeState._2 === 0)
-
-
-  }
-
-}
+// step-1-1
+class CarSuite extends Suite
